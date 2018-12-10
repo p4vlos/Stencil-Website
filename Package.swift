ea90eb616cbc7932ecce95c1cdd565ef993d5135
Pavlos Nicolaou <pavlosnicolaou@Pavloss-MacBook-Pro.local>
2018-12-10 16:19:54 +0000

    crating a website using Stencil

---
+++ Package.swift
@@ -0,0 +1,14 @@
+// swift-tools-version:4.1
+import PackageDescription
+
+let package = Package(
+  name: "KituraWebInterface",
+  dependencies: [
+    .package(url: "https://github.com/IBM-Swift/Kitura.git", .upToNextMinor(from: "2.4.1")),
+    .package(url: "https://github.com/IBM-Swift/HeliumLogger.git", .upToNextMinor(from: "1.7.1")),
+    .package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git", .upToNextMinor(from: "2.1.0")),
+    ],
+  targets: [
+    .target(name: "KituraWebInterface", dependencies: ["Kitura" , "HeliumLogger", "CouchDB"])
+  ]
+)
